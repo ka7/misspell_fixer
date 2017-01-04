456f348a00232c57c462ead5379d76de5a9184d8
klemens <ka7@github.com>
2017-01-04 23:02:09 +0100

    try to make travis check my own code with misspell-fixer, too(6)

--- test/self_spelling_test.sh
+++ test/self_spelling_test.sh
@@ -28,7 +28,8 @@ setUp(){
 # run over own code, assume zero errors.
 testSelf(){
         ./misspell-fixer.sh -s -D $TEMP/self/ > $TEMP/self/spelling.txt
-        if ( $(wc -l $TEMP/self/spelling.txt) <> 0 )
+        if [[ $(wc -l $TEMP/self/spelling.txt) <> 0 ]]
+        then
            cat $TEMP/self/spelling.txt
         fi
 	assertTrue $(cat $TEMP/self/spelling.txt | grep "^+" | wc -l) 0
