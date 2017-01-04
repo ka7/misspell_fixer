3e9c1e5779aa670635358bbb47284a6ecc30a0b5
klemens <ka7@github.com>
2017-01-04 23:12:09 +0100

    try to make travis check my own code with misspell-fixer, too(8)

--- test/self_spelling_test.sh
+++ test/self_spelling_test.sh
@@ -20,14 +20,13 @@ setUp(){
         rm $TEMP/self/test/expected/
         rm -R $TEMP/self/test/dict/*.dict
         rm -R $TEMP/self/.git
-        rm -R $TEMP/self/test/stubs/
-        rm $TEMP/self/test/expected*
+        rm -R $TEMP/self/test/stubs
 	set -f
 }
 
 # run over own code, assume zero errors.
 testSelf(){
-        ./misspell-fixer.sh -s -D $TEMP/self/ > $TEMP/self/spelling.txt
+        . misspell_fixer.sh -s -D $TEMP/self/ > $TEMP/self/spelling.txt
         echo "*** those errors found: ***"
         cat $TEMP/self/spelling.txt
 	assertTrue $(cat $TEMP/self/spelling.txt | grep "^+" | wc -l) 0
